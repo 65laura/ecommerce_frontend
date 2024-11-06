f7b31d9bb3aca27907307ba4df6cb79aaeeed06e
65laura <ishimwelauraceline@gmail.com>
2024-11-06 14:17:14 +0200

    quick fix

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -15,7 +15,7 @@ const LandingPage = () => {
     <div>
       <Navbar />
       <header className="hero">
-        <h2>Welcome to Laura's store</h2>
+        <h2>Welcome to Shop Mate</h2>
         <p>Your one-stop shop for everything!</p>
       </header>
       <section className="products">
