6ba8c871de6c0834f2cd6dd5b66557055980b298
65laura <ishimwelauraceline@gmail.com>
2024-11-14 19:42:38 +0200

    ft:add links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,27 @@
-
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>ShopMate</h1>
-      <ul>
-        <li>Home</li>
-        <li>Products</li>
-        <li>Cart</li>
-        <li>Contact</li>
+      <div className="navbar-logo">
+        <img src="/public/img.png" alt="ShopMate Logo" className="navbar-logo-img" />
+        <Link to="/" className="navbar-title">ShopMate</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/contactPage">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
