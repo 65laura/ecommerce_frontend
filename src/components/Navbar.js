239542ff7000adfb1c66d7ad9e3918d28af7dc85
65laura <ishimwelauraceline@gmail.com>
2025-01-17 18:40:57 -0600

    ft:profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,6 +23,9 @@ const Navbar = () => {
         <li>
           <Link to="/contact">Contact</Link>
         </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
       </ul>
     </nav>
   );
